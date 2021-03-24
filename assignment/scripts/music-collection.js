30e0c0d3c63e9f7a2c03b5d692de5a2acaa9ec92
Reese Avery Kling <raveryk@gmail.com>
2021-03-23 20:29:15 -0500

    addToCollection function created // about to test

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,3 +2,13 @@ console.log('***** Music Collection *****')
 
 
 let collection = [];
+
+function addToCollection( title, artist, yearPublished ) {
+  let album = {
+    title: " ",
+    artist: " ",
+    yearPublished: " "
+  } //end of object
+  collection.push(album);
+  return album;
+} //end addToCollection
