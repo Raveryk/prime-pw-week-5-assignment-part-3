401d0a137fee559a952708eb22401d04ee13c7df
Reese Avery Kling <raveryk@gmail.com>
2021-03-23 20:44:36 -0500

    tested addToCollection function

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -4,11 +4,20 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection( title, artist, yearPublished ) {
-  let album = {
-    title: " ",
-    artist: " ",
-    yearPublished: " "
+  let favAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
   } //end of object
-  collection.push(album);
-  return album;
+  collection.push(favAlbum);
+  return favAlbum;
 } //end addToCollection
+
+console.log( addToCollection( 'These Are The Vistas', 'The Bad Plus', 2003) );
+console.log( addToCollection( 'Self Titled', 'Halloween, Alaska', 2003) );
+console.log( addToCollection( 'Mutable Set', 'Blake Mills', 2020) );
+console.log( addToCollection( 'Please Refrain From Fronting', 'Happy Apple', 2001) );
+console.log( addToCollection( 'Free Love', 'Sylvan Esso', 2020) );
+console.log( addToCollection( 'Heigh Ho', 'Blake Mills', 2014) );
+
+console.log(collection);
